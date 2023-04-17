809cbd39ef28d7f48cd326c2ba4f9e3b43d8d1e8
Simonebaca <simobaco99@gmail.com>
2023-04-17 17:25:45 +0200

    Add strucutre and Api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,26 @@
 <script>
+import { store } from "../store.js";
+import axios from "axios";
 export default {
     data() {
         return {
+            listCards: [],
+            store,
         }
+    },
+    created() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0').then((res) => {
+            console.log(res.data.data);
+            this.store.listCards = res.data.data
+        })
     },
 }
 </script>
 
 <template>
-    
+    <div id="cards-list">
+
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
